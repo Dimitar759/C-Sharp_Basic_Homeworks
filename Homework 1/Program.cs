6da75e47d71a280ee2c9098dc515d7bf139c8cde
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-02-27 00:48:50 +0100

    C# first homework done

---
+++ Homework 1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeworkOne{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  *");
+            Console.WriteLine("  *");
+            Console.WriteLine(" ***");
+            Console.WriteLine("* * *");
+            Console.WriteLine("  *");
+            Console.WriteLine("  *");
+            Console.WriteLine(" * *");
+            Console.WriteLine("*   *");
+
+            Console.WriteLine("Enter the man's name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"The man's name is {name}");
+            Thread.Sleep(5000);
+
+        }
+    }
+}
