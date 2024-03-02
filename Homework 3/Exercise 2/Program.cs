9b7e98e399e50afd1a41898a0a41a2f7c9b4b66c
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-02 21:23:58 +0100

    Homework 3 finished

---
+++ Homework 3/Exercise 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= number1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Input another number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = number2; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Homework 3/RealCalculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+
+namespace RealCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            double number1;
+            while (!double.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("Invalid input, enter a number again:");
+            }
+
+            Console.WriteLine("Input another number:");
+            double number2;
+            while (!double.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("Invalid input, enter a number again:");
+            }
+
+            Console.WriteLine("Choose one of the following operations:  +, - , * , /");
+            char operand;
+            while (!char.TryParse(Console.ReadLine(), out operand))
+            {
+                Console.WriteLine("Invalid input, enter an operation again:");
+            }
+
+
+            double sum = 0;
+
+            switch (operand)
+            {
+                case '+':
+                    sum = number1 + number2;
+                    Console.WriteLine(sum);
+                    break;
+                case '-':
+                    sum = number1 - number2;
+                    Console.WriteLine(sum);
+                    break;
+                case '*':
+                    sum = number1 * number2;
+                    Console.WriteLine(sum);
+                    break;
+                case '/':
+                    sum = number1 / number2;
+                    Console.WriteLine(sum);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation selected");
+                    break;
+            }
+
+        }
+    }
+}
