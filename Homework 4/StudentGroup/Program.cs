afbba73b9d7e02d0ab77f69bec178e92ca325acf
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-05 21:54:02 +0100

    Exercise 1 complete

---
+++ Homework 4/StudentGroup/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Matt", "Damian", "Silvester", "Stacey", "Mary" };
+            string[] studentsG2 = { "Ben", "Kevin", "Qwen", "Max", "Ester" };
+
+            Console.WriteLine("Enter the number 1 or the number 2");
+            
+            
+        }
+    }
+}
---
+++ Homework 4/SumOfEven/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = new int[6];
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+
+                Console.WriteLine($"Enter the {(i + 1)}th number:");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Please enter a number.");
+                    Console.WriteLine("");
+                    continue;
+                }
+                else if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    Console.WriteLine("");
+                    continue;
+                }
+                else
+                {
+                    arrayOfNumbers[i] = number;
+                }
+
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                if (arrayOfNumbers[i] % 2 == 0)
+                {
+                    sum += arrayOfNumbers[i];
+                }
+            }
+
+            Console.WriteLine($"The sum of all even numbers is: {sum}");
+        }
+    }
+}
