1aadbf5c1b4bea92daed1ab24f99049352f8cf5e
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-09 22:45:26 +0100

    Exercise 1 complete

---
+++ Homework 5/Exercise 1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your birth year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input your birth month (number):");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the day of your birth (number):");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(year, month, day);
+            
+
+            int age = CalculateAge(birthDate);
+
+            Console.WriteLine($"Your age is: {age}");
+        }
+
+        public static int CalculateAge(DateTime birthDate)
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthDate.Year;
+
+           
+            if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+            {
+                age--; // Subtract one year if the birthday hasn't occurred yet this year
+            }
+
+            return age;
+        }
+    }
+}
