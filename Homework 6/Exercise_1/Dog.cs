445263bf7fa168a8e9d3a37b568ad2dff9de3641
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-12 18:16:41 +0100

    Homework 6 finished

---
+++ Homework 6/Exercise_1/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        public string Color { get; set; }
+
+        public string Eat()
+        {
+            return $"The dog named {Name} who is a {Breed} breed with {Color} color is now eating";
+        }
+
+        public string Play()
+        {
+            return $"The dog named {Name} who is a {Breed} breed with {Color} color is now playing";
+        }
+
+        public string ChaseTail()
+        {
+            return $"The dog named {Name} who is a {Breed} breed with {Color} color is now chasing its tail";
+        }
+    }
+}
---
+++ Homework 6/Exercise_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Cryptography;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dogBreed;
+            string dogName;
+            string dogColor;
+            while (true)
+            {
+                Console.WriteLine("Enter the dog's name:");
+                dogName = Console.ReadLine();
+
+                if (dogName == null || dogName.Trim() == "")
+                {
+                    Console.WriteLine("Please enter a valid name for the dog.");
+                    continue;
+                }
+
+                Console.WriteLine("Enter the dog's breed:");
+                dogBreed = Console.ReadLine();
+
+                if (dogBreed == null || dogBreed.Trim() == "")
+                {
+                    Console.WriteLine("Please enter a valid breed for the dog.");
+                    continue; 
+                }
+
+                Console.WriteLine("Enter the dog's color:");
+                dogColor = Console.ReadLine();
+
+                if (dogColor == null || dogColor.Trim() == "")
+                {
+                    Console.WriteLine("Please enter a valid color for the dog.");
+                    continue; 
+                }
+
+                break;
+            }
+
+            Dog dog1 = new Dog()
+            {
+                Name = dogName,
+                Breed = dogBreed,
+                Color = dogColor
+
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Choose one of the numbers: 1, 2, or 3");
+
+                int number;
+                if (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Please enter a valid number!");
+                    continue;
+                }
+                else if (number < 1 || number > 3)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 3!");
+                    continue;
+                }
+                else if(number == 1) 
+                {
+                    Console.WriteLine(dog1.Eat());
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine(dog1.Play());
+                }else if (number == 3)
+                {
+                    Console.WriteLine(dog1.ChaseTail());
+                }
+
+                break;
+            }
+        }
+    }
+}
