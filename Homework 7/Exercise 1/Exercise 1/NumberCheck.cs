441d37915a0f10af7adcf171ae192bcad0073620
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-14 21:46:29 +0100

    Homework 7 done

---
+++ Homework 7/Exercise 1/Exercise 1/NumberCheck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class NumberCheck
+    {
+        public void NumberStats(double number)
+        {
+            string positiveOrNegative = "";
+            if(number > 0) 
+            {
+                positiveOrNegative = "Positive";
+
+            }else if(number < 0)
+            {
+                positiveOrNegative = "Negative";
+
+            }else if(number == 0) 
+            {
+                positiveOrNegative = "Neither negative or positive (0)";
+            }
+
+
+            string oddOrEven = ""; 
+            if(number % 2 == 0)
+            {
+                oddOrEven = "Even";
+
+            }else if (number % 2 != 0)
+            {
+                oddOrEven = "Odd";
+            }
+
+            string decimalOrInteger = "";
+            if(number % 1 == 0)
+            {
+                decimalOrInteger = "Integer";
+
+            }else if( number % 1 != 0)
+            {
+                decimalOrInteger = "Decimal";
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"Stats for number: {number}");
+            Console.WriteLine($"• {positiveOrNegative}");
+            Console.WriteLine($"• {oddOrEven}");
+            Console.WriteLine($"• {decimalOrInteger}");
+        }
+    }
+}
+
---
+++ Homework 7/Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using Exercise_1;
+
+namespace Exercise_1
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input) || !double.TryParse(input, out double number))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input! Please enter a valid number.");
+                    Console.WriteLine("");
+                    continue;
+                }
+
+                NumberCheck checker = new NumberCheck();
+                checker.NumberStats(number);
+
+                while (true)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("Do you want to enter another number? - Enter y/n");
+                    string inputAgain = Console.ReadLine().ToLower();
+
+                    if (string.IsNullOrEmpty(inputAgain))
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Enter a valid input please");
+                    }
+                    else if (inputAgain == "n")
+                    {
+                        return;
+                    }
+                    else if (inputAgain == "y")
+                    {
+                        Console.WriteLine("");
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Invalid input! Please enter 'y' or 'n'.");
+
+                    }
+                }
+            }
+
+        }
+    }
+}
