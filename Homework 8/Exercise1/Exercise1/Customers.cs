17326b375b01f8b57304aaab0c314d36cd65342c
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-18 01:00:06 +0100

    Homework 8 done

---
+++ Homework 8/Exercise1/Exercise1/Customers.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Customers
+    {
+        public string CardNumber { get; set; }
+        private int Pin { get; set; }
+        public string FullName { get; set; }
+        private int Balance { get; set; }
+
+        public Customers(string cardNumber, int pin, string fullName, int balance)
+        {
+            CardNumber = cardNumber;
+            Pin = pin;
+            FullName = fullName;
+            Balance = balance;
+        }
+
+        public Customers()
+        {
+        }
+
+        public static Customers Authenticate(string cardNumber)
+        {
+            if (cardNumber == "1234-1234-1234-1234" || cardNumber == "8390-8390-8390-8390" ||
+                cardNumber == "7379-7379-7379-7379" || cardNumber == "2865-2865-2865-2865")
+            {
+                if (cardNumber == "1234-1234-1234-1234")
+                {
+                    Console.WriteLine("Welcome Max Tennyson");
+                }
+                else if (cardNumber == "8390-8390-8390-8390")
+                {
+                    Console.WriteLine("Welcome Kevin Levin");
+                }
+                else if (cardNumber == "7379-7379-7379-7379")
+                {
+                    Console.WriteLine("Welcome Gwen Stacey");
+                }
+                else if (cardNumber == "2865-2865-2865-2865")
+                {
+                    Console.WriteLine("Welcome Becky Smith");
+                }
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Please input your pin:");
+                string pinEntered = Console.ReadLine();
+                if (!string.IsNullOrEmpty(pinEntered) && int.TryParse(pinEntered, out int pin))
+                {
+                    if ((cardNumber == "1234-1234-1234-1234" && pin == 4321) ||
+                    (cardNumber == "8390-8390-8390-8390" && pin == 1835) ||
+                    (cardNumber == "7379-7379-7379-7379" && pin == 8653) ||
+                    (cardNumber == "2865-2865-2865-2865" && pin == 7294))
+                    {
+
+                        Console.WriteLine("");
+                        switch (cardNumber)
+                        {
+                            case "1234-1234-1234-1234":
+                                return new Customers("1234-1234-1234-1234", 4321, "Max Tennyson", 1000);
+                            case "8390-8390-8390-8390":
+                                return new Customers("8390-8390-8390-8390", 1835, "Kevin Levin", 2000);
+                            case "7379-7379-7379-7379":
+                                return new Customers("7379-7379-7379-7379", 8653, "Gwen Stacey", 3000);
+                            case "2865-2865-2865-2865":
+                                return new Customers("2865-2865-2865-2865", 7294, "Becky Smith", 4000);
+                            default:
+                                return null;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid pin");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid card number");
+            }
+            return null;
+        }
+
+        public string GetBalance()
+        {
+            return $"{Balance}";
+        }
+
+        public string Withdraw(int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return $"Withdrawal successful. Amount withdrawn: {amount}. New balance: {Balance}$";
+            }
+            else
+            {
+                return "You aint got that much money bro LOL, work more!";
+            }
+        }
+
+        public string Deposit(int amount)
+        {
+            Balance += amount;
+            return $"Deposit successful. New balance: {Balance}$";
+        }
+    }
+}
+
+
---
+++ Homework 8/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your card number:");
+            string cardNumber = Console.ReadLine();
+
+            Customers customer = Customers.Authenticate(cardNumber);
+
+            if (customer != null)
+            {
+                Console.WriteLine($"Welcome, {customer.FullName}!");
+
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Cash Withdrawal");
+                Console.WriteLine("3. Cash Deposition");
+
+                string choiceInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(choiceInput) && int.TryParse(choiceInput, out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine($"Your current balance is: {customer.GetBalance()}");
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("Enter the amount to withdraw:");
+                            string withdrawInput = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(withdrawInput) && int.TryParse(withdrawInput, out int withdrawAmount))
+                            {
+                                if (withdrawAmount > 0)
+                                {
+                                    Console.WriteLine("");
+                                    string withdrawalResult = customer.Withdraw(withdrawAmount);
+                                    Console.WriteLine(withdrawalResult);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("Enter an amount greater than 0.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Invalid input. Please enter a valid amount.");
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("");
+                            Console.WriteLine("Enter the amount to deposit:");
+                            string depositInput = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(depositInput) && int.TryParse(depositInput, out int depositAmount))
+                            {
+                                if (depositAmount > 0)
+                                {
+                                    Console.WriteLine("");
+                                    string depositResult = customer.Deposit(depositAmount);
+                                    Console.WriteLine(depositResult);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("Enter an amount greater than 0.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Invalid input. Please enter a valid amount.");
+                            }
+                            break;
+                        default:
+
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid choice. Enter a number between 1 and 3!");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Authentication failed.");
+            }
+
+        }
+    }
+}
+
