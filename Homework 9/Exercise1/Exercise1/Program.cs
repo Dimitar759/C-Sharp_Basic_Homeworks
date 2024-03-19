d9b5f6b7e598cb40b5a93e6334b04fd7ebe299f7
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-19 10:32:26 +0100

    Exercise 1 complete

---
+++ Homework 9/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            while (true)
+            {
+                Console.WriteLine("Please input a number:");
+                string num = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(num) && int.TryParse(num, out int number))
+                {
+                    queue.Enqueue(number);
+
+                    Console.WriteLine("Do you wish to enter another number? Enter y or n:");
+                    string yOrN = Console.ReadLine().ToLower();
+
+                    if (yOrN != "y" && yOrN != "n")
+                    {
+                        Console.WriteLine("Wrong input!");
+                        return;
+                    }
+                    else if (yOrN == "n")
+                    {
+                        break;
+                    }
+                    else if(yOrN == "y")
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("\nThe numbers you inputted are:");
+
+            foreach (int n in queue)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
